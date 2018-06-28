447131062bc0d6878943bff90c6ae41941530cad
Ane Itodo <36627301+aneitodo@users.noreply.github.com>
2018-06-28 13:44:18 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Head from "./Head";
+import Board from "./Board";
+
+class App extends React.Component {
+  render () {
+    return(
+    
+          <Board />
+         
+        </div>
+    </div>
+    );
+  }
+  }
+
+  export default  App;

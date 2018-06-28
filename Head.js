17388692a839a87e4ad246f9cac220e7ca0095e1
Ane Itodo <36627301+aneitodo@users.noreply.github.com>
2018-06-28 13:52:05 +0100

    Create Head.js

---
+++ Head.js
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class Head extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Burberry</h1>
+        </header>
+       
+      </div>
+    );
+  }
+}
+
+export default Head;
